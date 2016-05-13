d1ee631ec032cadbe8d09b9aa8a6f39e5601cf22
Terence Baker <terence@bulbstudios.com>
2016-05-13 08:39:08 +0100

    Found a way to tidy up the url creation

--- JustEat/Service/JustEatService.swift
+++ JustEat/Service/JustEatService.swift
@@ -10,8 +10,11 @@ import Alamofire
 
 class JustEatService {
 
+    //Not happy with this class. Feel like there is a better appraoch.
     private static let AUTH_CODE = "VGVjaFRlc3RBUEk6dXNlcjI="
-    private static let RESTURANT_URL = "https://public.je-apis.com/restaurants"
+    private static let BASE_URL = "https://public.je-apis.com"
+    private static let RESTURANT_URL = "\(BASE_URL)/restaurants"
+    private static let RESTURANT_QUERY = "q"
 
     //Create a single instance of the Manager so that we don't need to setup the headers for every request
     static let manager: Manager = {
@@ -31,13 +34,13 @@ class JustEatService {
     //URL encode the postcode and provide a JSON observerable for the request
     static func resturantSearchObservable(postcode: String) -> Observable<AnyObject>? {
 
-        guard let urlPostcode = postcode.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet()) else {
+        guard let urlPostcode = postcode.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet()), let components = NSURLComponents(string: JustEatService.RESTURANT_URL) else {
 
             print("Error creating URL encoded post code")
             return nil
         }
 
-        let url = "\(JustEatService.RESTURANT_URL)?q=\(urlPostcode)"
-        return JustEatService.manager.rx_JSON(.GET, url)
+        components.queryItems = [NSURLQueryItem(name: JustEatService.RESTURANT_QUERY, value: urlPostcode)]
+        return JustEatService.manager.rx_JSON(.GET, components.URLString)
     }
 }
